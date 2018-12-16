d0dac2ba7358470d708064107af187168545b302
GAOHaoxiang95 <40728832+GAOHaoxiang95@users.noreply.github.com>
2018-12-16 16:55:30 +0000

    finish the transcript

--- spider.py
+++ spider.py
@@ -1,23 +1,28 @@
 from bs4 import BeautifulSoup
 import requests
+import re
 
 feasible = 'https://www.mywenxue.com'
 start_url = 'https://www.mywenxue.com/xiaoshuo/132/132746/55793238.htm'
 
 url = start_url
+with open('novel.txt', 'w', encoding = 'utf8') as f:
+	while re.match('.*Index\.htm$', url) == None:
+		r = requests.get(url)
 
-while url
-r = requests.get(url)
+		s = BeautifulSoup(r.text, 'lxml')
+		next = s.find(class_='r').a['href']
+		url = feasible + next
 
-s = BeautifulSoup(r.text, 'lxml')
-next = s.find(class_='r').a['href']
-url = feasible + next
-
-for article in s.find(class_='txt'):
-	if article.string != None:
-		#print(article.string)
-		print('dd')
-
+		title = s.h2.text
+		print(title)
+		f.write(title + '\n')
+		for article in s.find(class_='txt'):
+			if article.string != None:
+				f.write(article.string + '\n')
+				
+f.close()
+			
 
 
 
